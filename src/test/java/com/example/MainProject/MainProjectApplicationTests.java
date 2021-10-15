3fffcd8cf5531af344c6874cd0264c0223a99fd9
rafiqul0396 <rafiqul0396>
2021-10-15 11:44:58 +0530

    this is first commit

---
+++ src/test/java/com/example/MainProject/MainProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MainProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
